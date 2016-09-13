2c0459d6a02c39910b00460f8c1f7873175977db
ivanivanyuk <von.asar@gmail.com>
2016-09-13 22:37:39 +0300

    Индексная страница теперь ссылается на форму создания

--- task2/views/site/index.php
+++ task2/views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
     <div class="body-content">
         <div class="row text-center">
-            <a href="/index.php?r=user">Ссылка на форму с валидацией</a>
+            <a href="/index.php?r=user/create">Ссылка на форму с валидацией</a>
         </div>
     </div>
 </div>
