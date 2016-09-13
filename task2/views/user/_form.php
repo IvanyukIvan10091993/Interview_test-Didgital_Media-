4bfea1777186fafa6ba269a94cb8a745c1146f52
ivanivanyuk <von.asar@gmail.com>
2016-09-13 20:22:46 +0300

    Включена ajax валидация

--- task2/views/user/_form.php
+++ task2/views/user/_form.php
@@ -10,7 +10,10 @@ use yii\widgets\ActiveForm;
 
 <div class="user-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin([
+        'id' => 'contact-form',
+        'enableAjaxValidation' => true,
+    ]); ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
